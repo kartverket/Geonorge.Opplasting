5deb9d4970979214915727d310517a6be304739f
Ole <ole@arkitektum.no>
2023-06-12 10:49:59 +0200

    Added header geonorge webcomp and sass support

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { MainNavigation } from '@kartverket/geonorge-web-components/MainNavigation';
+ import style from './App.module.scss';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>
-          Opplasting
-        </h1>
-      </header>
+    <div className={style.kartkatalogen}>
+     <main-navigation environment="dev"></main-navigation>
+      
     </div>
   );
 }
