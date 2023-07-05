d5816ab977df090b2eedd6d8d83b627205d941de
Dag Olav Dahle <dagolav@arkitektum.no>
2023-07-05 08:23:09 +0200

    Format date

--- src/components/partials/FilelistItem.js
+++ src/components/partials/FilelistItem.js
@@ -9,12 +9,22 @@ import { Link } from "react-router-dom";
 
 const FilelistItem = (props) => {
 
+  const formatDate = (date) => {
+    if (date !== null) {
+        const dateArray = date.split("-");
+        const day = dateArray[2].substring(0, 4).split("T");
+        var timeArray = dateArray[2].split("T");
+        var time = timeArray[1].substring(0, 5);
+        return day[0] + "." + dateArray[1] + "." + dateArray[0] + " " + time;
+    }
+};
+
   const url = `https://opplasting.dev.geonorge.no/api/Dataset/download-file/${props.file.id}`
   
     return (
           <tr>
             <td><Link to={url}>{props.file.fileName}</Link></td>
-            <td>{props.file.date}</td>
+            <td>{formatDate(props.file.date)}</td>
             <td>{props.file.status}</td>
           </tr>
     );
