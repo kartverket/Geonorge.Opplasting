a38cc6c8adb30af1ecad078d217f33dd64ecfeb8
Dag Olav Dahle <dagolav@arkitektum.no>
2023-07-06 18:51:08 +0200

    Update file status

--- src/components/partials/FilelistItem.js
+++ src/components/partials/FilelistItem.js
@@ -1,13 +1,44 @@
 // Dependencies
 import React from "react";
-
+import axios from 'axios';
 // Geonorge WebComponents
 // eslint-disable-next-line no-unused-vars
 import { ContentContainer, HeadingText, breadcrumbList } from "@kartverket/geonorge-web-components";
 import { Link } from "react-router-dom";
+import { useState, useEffect } from 'react';
 
 
 const FilelistItem = (props) => {
+
+  const [statusValue, setStatusValue] = useState(props.file.status);
+
+  useEffect(() => () => {
+    setStatusValue(props.file.status);
+  }, [props.file.status]);
+
+  const handleSubmit = async (event) => {
+    setStatusValue(event.target.value)
+    event.preventDefault();
+    const datasetForm = {
+      status : event.target.value
+    };
+    //todo improve dialog?
+    try {
+       const response = await axios.put(`https://opplasting.dev.geonorge.no/api/Dataset/file/${props.file.id}`, datasetForm);
+
+       if (response.data) {
+          //alert("Lagret"); 
+       }
+    } catch (error) {
+      alert("Feilet" + error);
+
+       if (error.response?.data) {
+        alert("Feilet" + error.response?.data);
+       } else {
+        alert("Feilet" + error.message);
+       }                
+    }
+ };
 
   const formatDate = (date) => {
     if (date !== null) {
@@ -18,7 +49,7 @@ const FilelistItem = (props) => {
         return day[0] + "." + dateArray[1] + "." + dateArray[0] + " " + time;
     }
 };
- 
+
   const url = `https://opplasting.dev.geonorge.no/api/Dataset/download-file/${props.file.id}`
   
     return (
@@ -27,7 +58,7 @@ const FilelistItem = (props) => {
             <td>{formatDate(props.file.date)}</td>
             <td>
             <gn-select block="">
-            <select value={props.file.status}>
+            <select value={statusValue}  onChange={handleSubmit}>
               <option value={props.fileStatuses.Submitted}>{props.fileStatuses.Submitted}</option>
               <option value={props.fileStatuses.InProcess}>{props.fileStatuses.InProcess}</option>
               <option value={props.fileStatuses.Valid}>{props.fileStatuses.Valid}</option>
